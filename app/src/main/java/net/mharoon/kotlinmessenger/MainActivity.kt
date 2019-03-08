8e65f2ae296e413f194f02b40a296ca4795d4dcd
mharoon <moe@mharoon.net>
2019-03-08 09:45:02 -0800

    c21

--- app/src/main/java/net/mharoon/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/net/mharoon/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +17,20 @@ class MainActivity : AppCompatActivity() {
 
         //val username = usernameET.text.toString()
 
-
         registerBtn.setOnClickListener {
             val email = emailET.text.toString()
             val pwd = passwordET.text.toString()
             Log.d("MainActivity","Email is "+ email)
             Log.d("MainActivity","Password: $pwd")
+
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email,pwd)
+                .addOnCompleteListener {
+                    if (! it.isSuccessful) return@addOnCompleteListener
+                    Log.d("MainActivity","Successfully Logged In with uid: ${it.result.user.uid}")
+                }
+
+
         }
 
         registerTV.setOnClickListener {
